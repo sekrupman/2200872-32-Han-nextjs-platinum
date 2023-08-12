4f5a3904b9828015bec1003c8c3e404fd9699141
agnes-septilia <agnes.septilia@gmail.com>
2023-08-12 22:47:34 +0700

    revert _app.js, index.js, and next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    images: {
-      unoptimized: true,
-    },
-    env: {
-      REACT_APP_BE_URL: 'https://gifted-mittens-mite.cyclic.app'
-    }
+  images: {
+    unoptimized: true,
+  },
+  env: {
+    REACT_APP_BE_URL: 'https://gifted-mittens-mite.cyclic.app'
   }
+} 
